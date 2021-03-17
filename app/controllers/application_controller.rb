a7ed614a59a3fd16cfdab13fecce4bddb5715d4f
Tinkergit <nicholas.stagno@gmail.com>
2021-03-17 17:20:31 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
-
-require_relative '../../config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -10,4 +7,53 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles=Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article=Article.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    attrs = params
+    @article=Article.create(attrs)
+
+    redirect to "/articles/#{@article.id}"
+
+  end
+
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article=Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article=Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect to "/articles"
+  end
+
 end
+
+
+
+
+
+
